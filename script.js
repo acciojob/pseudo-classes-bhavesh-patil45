da9aee433efa2a215c91896a2e0d24421503a9c8
BHAVESH SANJAY PATIL <154696194+bhavesh-patil45@users.noreply.github.com>
2025-11-02 12:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your JS code here. If required.
+body {
+    background-color: lightblue;
+    font-family: Arial, sans-serif;
+}
+
+/* Paragraph with red text */
+.highlight {
+    color: red;
+}
+
+/* List styling */
+ul {
+    list-style-type: disc;
+    padding-left: 40px;
+}
+
+/* Alternate list background colors */
+ul li:nth-child(odd) {
+    background-color: white;
+}
+
+ul li:nth-child(even) {
+    background-color: #6B7280; /* gray */
+    color: #222;
+}
+
+/* Special styling for third item */
+ul li:nth-child(3) {
+    background-color: pink;
+}
+
+/* Add spacing and width for list items */
+ul li {
+    padding: 10px;
+    width: 400px;
+    margin-bottom: 5px;
+    font-size: 20px;
+}
